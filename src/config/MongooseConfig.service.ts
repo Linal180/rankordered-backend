8144311ac53672253b9f87a48949a9cdd6647cdd
amirizzudin4 <amirizzudin4@gmail.com>
2022-03-14 00:58:46 +0800

    update mongoos config

--- src/config/MongooseConfig.service.ts
+++ src/config/MongooseConfig.service.ts
@@ -10,15 +10,23 @@ export class MongooseConfigSerive implements MongooseOptionsFactory {
     createMongooseOptions():
         | MongooseModuleOptions
         | Promise<MongooseModuleOptions> {
-        return {
-            uri: process.env.MONGO_USERNAME
-                ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
-                : `mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`,
+        const option: MongooseModuleOptions = {
+            uri: `mongodb://${process.env.DATABASE_URL}`,
+            dbName: process.env.DATABASE_NAME,
             connectionFactory: (connection: Connection) => {
                 // eslint-disable-next-line @typescript-eslint/no-var-requires
                 connection.plugin(require('mongoose-autopopulate'));
                 return connection;
             }
         };
+
+        if (process.env.MONGO_USERNAME) {
+            option.auth = {
+                username: process.env.MONGO_USERNAME,
+                password: process.env.MONGO_PASSWORD
+            };
+        }
+
+        return option;
     }
 }
