60c95116a85b181c6dfbce3db44382f2149487b5
amirizzudin4 <amirizzudin4@gmail.com>
2022-03-17 00:26:24 +0800

    update mongo config

--- src/config/MongooseConfig.service.ts
+++ src/config/MongooseConfig.service.ts
@@ -30,8 +30,6 @@ export class MongooseConfigSerive implements MongooseOptionsFactory {
             return connection;
         };
 
-        console.log(option);
-
         return option;
     }
 }
